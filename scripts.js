ac004de505f832c1471fa1ffb82866d218969fdc
SupaSibs <92636078+SupaSibs@users.noreply.github.com>
2021-11-19 01:47:16 -0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,10 +2,7 @@ function show() {
   let x = document.getElementById("x");
   if (x.style.display === "none") {
     x.style.display = "block"
-    x1.style.display = "none"
-    x2.style.display = "none"
-    x3.style.display = "none"
-    x4.style.display = "none"
+  
   }
   else {
     x.style.display = "none"
@@ -16,10 +13,7 @@ function show1() {
   let x1 = document.getElementById("x1");
     if (x1.style.display === "none") {
     x1.style.display = "block"
-    x.style.display = "none"
-    x2.style.display = "none"
-    x3.style.display = "none"
-    x4.style.display = "none"
+   
   }
   else {
     x1.style.display = "none"
@@ -29,10 +23,7 @@ function show2() {
     let x2 = document.getElementById("x2");
       if (x.style.display === "none") {
     x2.style.display = "block"
-    x.style.display = "none"
-    x1.style.display = "none"
-    x3.style.display = "none"
-    x4.style.display = "none"
+    
   }
   else {
     x2.style.display = "none"
@@ -42,10 +33,7 @@ function show3() {
     let x3 = document.getElementById("x3");
       if (x3.style.display === "none") {
     x3.style.display = "block"
-    x.style.display = "none"
-    x1.style.display = "none"
-    x2.style.display = "none"
-    x4.style.display = "none"
+   
   }
   else {
     x3.style.display = "none"
@@ -55,10 +43,7 @@ function show4() {
     let x4 = document.getElementById("x4");
       if (x4.style.display === "none") {
     x4.style.display = "block"
-    x1.style.display = "none"
-    x2.style.display = "none"
-    x3.style.display = "none"
-    x.style.display = "none"
+    
   }
   else {
     x4.style.display = "none"
