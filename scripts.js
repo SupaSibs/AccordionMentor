a05eaa0edc3a176a828e047d2e7275f07b192b5d
SupaSibs <92636078+SupaSibs@users.noreply.github.com>
2021-11-21 18:48:07 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,11 +4,11 @@ function show() {
   let x = document.getElementById("x");
   if (x.style.display === "none") {
     x.style.display = "block";
-  x.style.fontWeight = "bold"
+  
   }
   else {
     x.style.display = "none";
-    x.style.fontWeight = "bold"
+    
   }
   
 }
@@ -16,44 +16,44 @@ function show1() {
   let x1 = document.getElementById("x1");
     if (x1.style.display === "none") {
     x1.style.display = "block";
-    x1.style.fontWeight = "bold"
+   
   }
   else {
     x1.style.display = "none";
-    x1.style.fontWeight = "bold"
+   
   }
 }
 function show2() {
     let x2 = document.getElementById("x2");
       if (x2.style.display === "none") {
     x2.style.display = "block";
-    x2.style.fontWeight = "bold"
+    
   }
   else {}
     x2.style.display = "none";
-    x2.style.fontWeight = "bold"
+    
   }
 }
 function show3() {
     let x3 = document.getElementById("x3");
       if (x3.style.display === "none") {
     x3.style.display = "block";
-    x3.style.fontWeight = "bold"
+    
    
   }
   else {
     x3.style.display = "none";
-    x3.style.fontWeight = "bold"
+    
   }
 }
 function show4() {
     let x4 = document.getElementById("x4");
       if (x4.style.display === "none") {
     x4.style.display = "block";
-    x4.style.fontWeight = "bold"
+    
   }
   else {
     x4.style.display = "none";
-    x.style.fontWeight = "bold"
+    
   }
 }
