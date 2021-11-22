445ab3fed8186f7f0caea25facdd50353087bcc2
SupaSibs <92636078+SupaSibs@users.noreply.github.com>
2021-11-22 15:38:24 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,59 +1,115 @@
-
 "use strict";
+
 function show() {
+
   let x = document.getElementById("x");
+
   if (x.style.display === "none") {
+
     x.style.display = "block";
+
   
+
   }
+
   else {
+
     x.style.display = "none";
+
     
+
   }
+
   
+
 }
+
 function show1() {
+
   let x1 = document.getElementById("x1");
+
     if (x1.style.display === "none") {
+
     x1.style.display = "block";
+
    
+
   }
+
   else {
+
     x1.style.display = "none";
+
    
+
   }
+
 }
+
 function show2() {
+
     let x2 = document.getElementById("x2");
+
       if (x2.style.display === "none") {
+
     x2.style.display = "block";
+
     
+
   }
+
   else {
+
     x2.style.display = "none";
+
     
+
   }
+
 }
+
 function show3() {
+
     let x3 = document.getElementById("x3");
+
       if (x3.style.display === "none") {
+
     x3.style.display = "block";
+
     
+
    
+
   }
+
   else {
+
     x3.style.display = "none";
+
     
+
   }
+
 }
+
 function show4() {
+
     let x4 = document.getElementById("x4");
+
       if (x4.style.display === "none") {
+
     x4.style.display = "block";
+
     
+
   }
+
   else {
+
     x4.style.display = "none";
+
     
+
   }
+
 }
